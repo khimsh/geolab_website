7f0a5ea2f0643b1ecc85d96c6b37c909ad0e2d6a
khimsh <i.khimish@gmail.com>
2018-04-09 18:49:08 +0400

    implemented mobile menu functionality

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-	$('#burger-icon').click(function(){
-		$(this).toggleClass('active');
+
+	function toggleNav() {
+		$("#burger-icon").toggleClass('active');
 		$('.main-navigation').toggleClass('open');
-	});
+		$('.header__logo-img').toggleClass('header__logo-img--active');
+	};
+
+	// opens and closes mobile menu when burger-icon is clicked
+	$('#burger-icon').click(toggleNav);
+
+	// closes mobile menu when one of the nav-links is clicked
+	$(".nav-link").click(toggleNav);
 });
